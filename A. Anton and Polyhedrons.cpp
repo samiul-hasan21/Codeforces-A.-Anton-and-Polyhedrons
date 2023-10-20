7a9402594145b6462d28081298c9b8e0c42bcc20
Samiul Hasan <106056497+samiul-hasan21@users.noreply.github.com>
2023-10-20 21:56:59 +0600

    Add files via upload

--- A. Anton and Polyhedrons.cpp
+++ A. Anton and Polyhedrons.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k,sum=0;
+    cin>>k;
+    string s;
+    for(int i=1;i<=k;i++){
+        cin>>s;
+        if(s[0]=='T'){
+            sum+=4;
+        }
+        else if(s[0]=='C'){
+            sum+=6;
+        }
+        else if(s[0]=='O'){
+            sum+=8;
+        }
+        else if(s[0]=='D'){
+            sum+=12;
+        }
+        else if(s[0]=='I'){
+            sum+=20;
+        }
+    }
 
+    cout<<sum<<endl;
+    return 0;
+}
